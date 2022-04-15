2f5ad0f13280edc7d004db5712299533d64767b8
roeeash <92747574+roeeash@users.noreply.github.com>
2022-04-15 16:40:17 +0300

    updated

---
+++ gui/simple_gui.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nCmdShow)
+{
+    MessageBox(NULL, "Simple GUI PATTERN", "Simple GUI", MB_OK);
+    return 0;
+}
