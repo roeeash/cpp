eeb854aa0bec6c411aef276c60995f7f4807338a
roeeash <92747574+roeeash@users.noreply.github.com>
2022-05-02 13:01:16 +0300

    updated leetcode records

--- leetCode/swapNodesInPairs.cpp
+++ leetCode/swapNodesInPairs.cpp
@@ -8,45 +8,33 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    
-    void printList(ListNode* head){
-        ListNode* curr = head;
-        while(curr!=NULL){
-            cout<<curr->val<<" ";
-            curr=curr->next;
-        }
-        cout<<endl;
-    }
-    
-    
-    ListNode* swapPairs(ListNode* head) {
-        ListNode* curr = head;
-        ListNode* temp= NULL, *prev = NULL;
-        
-        if(curr == NULL || curr->next ==NULL)
+    ListNode *swapPairs(ListNode *head)
+    {
+        ListNode *curr = head;
+        ListNode *temp = NULL, *prev = NULL;
+
+        if (curr == NULL || curr->next == NULL)
             return curr;
-      
-        
-        while (curr && curr->next){
-            
-                temp = curr->next;
-                curr->next = temp->next;
-                temp->next = curr;
-                
-                if(!prev)
-                     head = temp;
-                else
-                    prev->next=temp;
-            
-                prev = curr;
-                curr=curr->next;
-            
+
+        while (curr && curr->next)
+        {
+
+            temp = curr->next;
+            curr->next = temp->next;
+            temp->next = curr;
+
+            if (!prev)
+                head = temp;
+            else
+                prev->next = temp;
+
+            prev = curr;
+            curr = curr->next;
         }
-        
+
         return head;
-        
-        
     }
 };
